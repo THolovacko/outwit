7a66c882fbecb615b2511ddc24300e5b4e2a1e89
Tom <tom.holovacko@gmail.com>
2024-06-05 12:23:46 -0400

    started handling resize events

--- src/main.c
+++ src/main.c
@@ -1,85 +1,77 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window (adapted for HTML5 platform)
-*
-*   NOTE: This example is prepared to compile for PLATFORM_WEB, and PLATFORM_DESKTOP
-*   As you will notice, code structure is slightly diferent to the other examples...
-*   To compile it for PLATFORM_WEB just uncomment #define PLATFORM_WEB at beginning
-*
-*   Example originally created with raylib 1.3, last time updated with raylib 1.3
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2015-2024 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
+#include <emscripten/emscripten.h>
+#include <emscripten/html5.h>
 
-#if defined(PLATFORM_WEB)
-    #include <emscripten/emscripten.h>
-#endif
+void update_draw_frame(void);
+EM_BOOL resize_callback(int event_type, const EmscriptenUiEvent *ui_event, void *user_data);
+void request_fullscreen();
 
-//----------------------------------------------------------------------------------
-// Global Variables Definition
-//----------------------------------------------------------------------------------
-const int screenWidth = 800;
-const int screenHeight = 450;
-
-//----------------------------------------------------------------------------------
-// Module functions declaration
-//----------------------------------------------------------------------------------
-void UpdateDrawFrame(void);     // Update and Draw one frame
-
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
-#if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
-#else
-    SetTargetFPS(60);   // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    // get current screen width and height
     {
-        UpdateDrawFrame();
+        double width;
+        double height;
+        emscripten_get_element_css_size("canvas", &width, &height);
+        int screen_width  = (int)width;
+        int screen_height = (int)height;
+        SetWindowState(FLAG_WINDOW_RESIZABLE);
+        InitWindow(screen_width, screen_height, GAME_TITLE);
     }
-#endif
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    emscripten_set_resize_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW, NULL, EM_FALSE, resize_callback);
+    emscripten_set_main_loop(update_draw_frame, 0, 1);
+
+    // this code should never be reached
+    CloseWindow();  // close window and OpenGL context
 
     return 0;
 }
 
-//----------------------------------------------------------------------------------
-// Module Functions Definition
-//----------------------------------------------------------------------------------
-void UpdateDrawFrame(void)
+void update_draw_frame(void)
 {
-    // Update
-    //----------------------------------------------------------------------------------
-    // TODO: Update your variables here
-    //----------------------------------------------------------------------------------
-
-    // Draw
-    //----------------------------------------------------------------------------------
     BeginDrawing();
+        ClearBackground(BLACK);
 
-        ClearBackground(RAYWHITE);
+        int screen_width  = GetScreenWidth();
+        int screen_height = GetScreenHeight();
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        const char *text = "Hello, World!";
+        int font_size    = 20;
+
+        int text_width = MeasureText(text, font_size);
+
+        int pos_x = (screen_width  - text_width) / 2;
+        int pos_y = (screen_height - font_size)  / 2;
+
+        DrawText(text, pos_x, pos_y, font_size, LIGHTGRAY);
 
     EndDrawing();
-    //----------------------------------------------------------------------------------
+}
+
+EM_BOOL resize_callback(int event_type, const EmscriptenUiEvent *ui_event, void *user_data) {
+    double width;
+    double height;
+
+    emscripten_get_element_css_size("canvas", &width, &height);
+    int screen_width  = (int)width;
+    int screen_height = (int)height;
+    SetWindowSize(screen_width, screen_height);
+
+    return EM_TRUE;
+}
+
+void request_fullscreen() {
+    EM_ASM({
+        var canvas = Module['canvas'];
+        if (canvas.requestFullscreen) {
+            canvas.requestFullscreen();
+        } else if (canvas.mozRequestFullScreen) { // Firefox
+            canvas.mozRequestFullScreen();
+        } else if (canvas.webkitRequestFullScreen) { // Chrome, Safari and Opera
+            canvas.webkitRequestFullScreen();
+        } else if (canvas.msRequestFullscreen) { // IE/Edge
+            canvas.msRequestFullscreen();
+        }
+    });
 }
